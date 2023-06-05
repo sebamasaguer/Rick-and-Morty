08c8439387185b047fcd24d0e2b34b7f7fb26480
sebamasaguer <sebamasaguer@gmail.com>
2023-06-05 16:42:33 -0300

    pimer commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+function Card({ name, gender, onClose, species, image }) {
+   return (
+      <div>
+         <button onClick={onClose}>X</button>
+         <h2>{name}</h2>
+         <h2>{species}</h2>
+         <h2>{gender}</h2>
+         <img  src={image} alt={name} />
+      </div>
+   );
+}
+
+
+export default Card;
---
+++ src/components/Cards.jsx
@@ -0,0 +1,23 @@
+import Card from './Card';
+
+function Cards({ characters }) {
+
+   return (
+      <div>
+         {
+            characters.map(({id, name, species, gender, image}) => {
+              return <Card
+               key={id}
+               name={name}
+               species={species}
+               gender={gender}
+               image={image}
+               onClose={() => window.alert('Emulamos que se cierra la card')}
+              />
+            })
+         }
+      </div>
+   )
+}
+
+export default Cards;
